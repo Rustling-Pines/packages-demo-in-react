e3c9c7f17417ad9f7ac4103acf9f268caadbb997
Pradeep Rajendran <rajendran@porziols.com>
2024-12-05 16:02:26 -0500

    liked to npm package -working version

---
+++ README.old.md
@@ -0,0 +1,3 @@
+# packages-demo-in-react
+
+A React project to install &amp; test the npm-packages before publishing to npm
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TsToJsonConverterDemo from './demos/ts-to-json-npm/ts-to-json.demo';
+
+const App: React.FC = () => {
+    return (
+        <>
+            <div>Hello, React 16.8.0!</div>
+            <TsToJsonConverterDemo />
+        </>
+    );
+};
+
+export default App;
