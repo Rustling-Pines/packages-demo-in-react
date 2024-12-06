2fe2beff76a0cc453fea733c8daa6fea9ab8ba82
Pradeep Rajendran <rajendran@porziols.com>
2024-12-05 19:23:16 -0500

    streamlined approach

---
+++ src/translations/login.t.ts
@@ -0,0 +1,10 @@
+import { ITranslations } from "@rustling-pines/ts-to-json";
+import { Locales } from ".";
+
+export const LoginTranslation: ITranslations<Locales> = {
+    key: 'LOGIN-TEXT',
+    'en-us': 'Login',
+    fr: 'Connexion',
+    de: 'Anmelden',
+    es: 'Iniciar sesión'
+};
---
+++ src/translations/welcome.t.ts
@@ -0,0 +1,10 @@
+import { ITranslations } from "@rustling-pines/ts-to-json";
+import { Locales } from ".";
+
+export const WelcomeTranslations: ITranslations<Locales> = {
+    key: 'WELCOME',
+    'en-us': 'Welcome',
+    fr: 'Bienvenue',
+    de: 'Willkommen',
+    es: 'bienvenida'
+};
